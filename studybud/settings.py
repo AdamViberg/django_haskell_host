64c174ae959111ab843840e5fac6cca70e1c765e
Adam Viberg <59213441+AdamViberg@users.noreply.github.com>
2022-07-09 13:00:46 +0100

    Update settings.py

--- studybud/settings.py
+++ studybud/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+=qc@-x9jihf+d9ke(625e3$tqoo&5qpt3&=uc(=o_hler)vyt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
